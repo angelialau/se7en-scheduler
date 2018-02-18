6dcc7612e258fd32b2b4bd485a61bf9e2a6511cb
angelialau <angelialkm@gmail.com>
2018-02-18 18:30:48 +0800

    placeholder navbar and new major components

--- frontend/scheduler/src/app/app.module.ts
+++ frontend/scheduler/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+// material.angular imports
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { NotifComponent } from './notif/notif.component';
+import { TimetableComponent } from './timetable/timetable.component';
+
+const ROUTES: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'timetable', component: TimetableComponent },
+  { path: 'notifications', component: NotifComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    NotifComponent,
+    TimetableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(ROUTES), 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
