65fb76cd54804e146606398e70bb601cfe2ebb96
nosyarlin <rayson.ljk@gmail.com>
2018-02-21 13:18:54 +0800

    added db connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var db = require("dbconnection");
+var db = require("./dbconnection");
 
 app.get("/", function(req, res){
 	res.send("Hi there");
