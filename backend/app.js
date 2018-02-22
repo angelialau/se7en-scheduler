94909fa513da37b6e0f24fd138f4f21cf9a2932b
nosyarlin <rayson.ljk@gmail.com>
2018-02-22 18:25:26 +0800

    added logger

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var app = express();
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger("common"));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
