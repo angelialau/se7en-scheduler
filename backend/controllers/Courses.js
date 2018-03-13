2b4141a23afab3acca8234fe9be4afe3fc3e4415
nosyarlin <rayson.ljk@gmail.com>
2018-03-13 10:30:20 +0800

    first commit

--- backend/controllers/Courses.js
+++ backend/controllers/Courses.js
@@ -7,17 +7,7 @@ var router = express.Router();
 // defining route for get all courses
 router.get('/', function(req, res, next) {
 	Course.getAllCourses(function(err, rows) {
-		if (err) {
-			err.success = false;
-			res.json(err);
-		} else {
-			if (!utils.isEmptyObject(rows)) {
-				rows.success = true;
-				res.json(rows);
-			} else {
-				res.json({"success":false, "message":"no rows found"});
-			}
-		}
+		utils.basicGetCallback(res, err, rows);
 	});
 });
 
@@ -25,17 +15,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id(\\d+)', function(req, res, next) {
 	if (req.params.id) {
 		Course.getCourseById(req.params.id, function(err, rows) {
-			if (err) {
-				err.success = false;
-				res.json(err);
-			} else {
-				if (!utils.isEmptyObject(rows)) {
-					rows[0].success = true;
-					res.json(rows[0]);
-				} else {
-					res.json({"success":false, "message":"no rows found"});
-				}
-			}
+			utils.basicGetCallback(res, err, rows);
 		});
 	} 
 });
@@ -44,16 +24,7 @@ router.get('/:id(\\d+)', function(req, res, next) {
 router.get('/BySchedule/:schedule_id(\\d+)', function(req, res, next) {
 	if (req.params.schedule_id) {
 		Course.getCourseBySchedule(req.params.schedule_id, function(err, rows) {
-			if (err) {
-				err.success = false;
-				res.json(err);
-			} else {
-				if (!utils.isEmptyObject(rows)) {
-					res.json(rows);
-				} else {
-					res.json({"success":false, "message":"no rows found"});
-				}
-			}
+			utils.basicGetCallback(res, err, rows);
 		});
 	} 
 });
@@ -94,12 +65,7 @@ router.post('/', function(req, res, next) {
 						create_count.insertId,
 						1,
 						function(update_err, update_count) {
-							if (update_err) {
-								res.json(update_err);
-							} else {
-								create_count.success = true;
-								res.json(create_count);
-							}
+							utils.basicPostCallback(res, update_err, create_count);
 						});
 				}
 			}
@@ -128,13 +94,7 @@ router.post('/Update', function(req, res, next) {
 			req.body.term,
 			req.body.instructors,
 			function(err, count) {
-				if (err) {
-					err.success = false;
-					res.json(err);
-				} else {
-					count.success = true;
-					res.json(count);
-				}
+				utils.basicPostCallback(res, err, count);
 			}
 		);
 	} else {
@@ -159,13 +119,7 @@ router.post('/Delete', function(req, res, next) {
 						req.body.id,
 						-1,
 						function(update_err, update_count) {
-							if (update_err) {
-								update_err.success = false;
-								res.json(update_err);
-							} else {
-								count.success = true;
-								res.json(count);
-							}
+							utils.basicPostCallback(res, update_err, count);
 						});
 				}
 			}
--- backend/controllers/Schedules.js
+++ backend/controllers/Schedules.js
@@ -6,17 +6,7 @@ var router = express.Router();
 // defining route for get all schedules
 router.get('/', function(req, res, next) {
 	Schedule.getAllSchedules(function(err, rows) {
-		if (err) {
-			err.success = false;
-			res.json(err);
-		} else {
-			if (!utils.isEmptyObject(rows)) {
-				rows.success = true;
-				res.json(rows);
-			} else {
-				res.json({"success":false, "message":"no rows found"});
-			}
-		}
+		utils.basicGetCallback(res, err, rows);
 	});
 });
 
@@ -24,7 +14,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id(\\d+)', function(req, res, next) {
 	if (req.params.id) {
 		Schedule.getScheduleById(req.params.id, function(err, rows) {
-			utils.basicGetCallback(res, err, rows)
+			utils.basicGetCallback(res, err, rows);
 		});
 	} 
 });
@@ -37,13 +27,7 @@ router.post('/', function(req, res, next) {
 			req.body.year, 
 			req.body.trimester,
 			function(err, count) {
-				if (err) {
-					err.success = false;
-					res.json(err);
-				} else {
-					count.success = true;
-					res.json(count);
-				}
+				utils.basicPostCallback(res, err, count);
 			}
 		);
 	} else {
@@ -61,13 +45,7 @@ router.post('/Update', function(req, res, next) {
 			req.body.year, 
 			req.body.trimester,
 			function(err, count) {
-				if (err) {
-					err.success = false;
-					res.json(err);
-				} else {
-					count.success = true;
-					res.json(count);
-				}
+				utils.basicPostCallback(res, err, count);
 			}
 		);
 	} else {
@@ -82,13 +60,7 @@ router.post('/Delete', function(req, res, next) {
 		Schedule.deleteSchedule(
 			req.body.id,
 			function(err, count) {
-				if (err) {
-					err.success = false;
-					res.json(err);
-				} else {
-					count.success = true;
-					res.json(count);
-				}
+				utils.basicPostCallback(res, err, count);
 			}
 		);
 	} else {
