fafe0b7c834c7071cbad22ddf354601defa16257
nosyarlin <rayson.ljk@gmail.com>
2018-03-15 20:01:18 +0800

    added test case for rowToJSON

--- backend/test/courseTest.js
+++ backend/test/courseTest.js
@@ -1,14 +1,29 @@
 var expect = require('chai').expect;
 var Course = require('../models/Course')
 
-describe('getCoursesForAlgo()', function () {
+describe('rowToJSON()', function () {
 	var output;
-	it('returns courses in correct format', function() {
+	it('transforms course to correct format', function() {
+
+			// Setup
+			course = {
+		        "id": 58,
+		        "schedule_id": 25,
+		        "term": 1,
+		        "course_no": "50.003",
+		        "course_name": "Elements of Software Construction",
+		        "core": 1,
+		        "no_classes": 3,
+		        "class_size": 50,
+		        "no_sessions": 3,
+		        "sessions_hrs": "2,2,1",
+		        "class_types": "Lecture,Lecture,Lecture",
+		        "instructors": "31,31,31",
+		        "split": "1,1,1"
+    		}
 
 			// Act
-			Course.getCoursesForAlgo(1, function(row){
-				output = row;
-			});
+			output = Course.rowToJSON(course);
 
 			// Assert
 			expect(true).to.be.equal(true);
