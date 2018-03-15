a042ccf2543b9ba0d184df114519fc92bff6a039
nosyarlin <rayson.ljk@gmail.com>
2018-03-15 21:01:02 +0800

    changed rowToJSON to return irrelevant key:values

--- backend/test/courseTest.js
+++ backend/test/courseTest.js
@@ -18,7 +18,7 @@ describe('rowToJSON()', function () {
 		        "no_sessions": 3,
 		        "sessions_hrs": "2,2,1",
 		        "class_types": "Lecture,Lecture,Lecture",
-		        "instructors": "31,31,31",
+		        "instructors": "31,32,33",
 		        "split": "1,1,1"
     		}
 
@@ -33,15 +33,15 @@ describe('rowToJSON()', function () {
 				  "sessions": 
 				   [ { 'class_type': 'Lecture',
 				       'time': '2',
-				       'instructors': ['31','31','31'],
+				       'instructors': ['31','32','33'],
 				       'split': '1' },
 				     { 'class_type': 'Lecture',
 				       'time': '2',
-				       'instructors': ['31','31','31'],
+				       'instructors': ['31','32','33'],
 				       'split': '1' },
 				     { 'class_type': 'Lecture',
 				       'time': '1',
-				       'instructors': ['31','31','31'],
+				       'instructors': ['31','32','33'],
 				       'split': '1' } 
 				    ] 
 			}
