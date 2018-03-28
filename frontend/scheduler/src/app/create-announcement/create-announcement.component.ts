1c052c371cefd2372d0f0fc19a6af744081ae899
angelialau <angelialkm@gmail.com>
2018-03-28 19:27:47 +0800

    integrated make announcement

--- frontend/scheduler/src/app/create-announcement/create-announcement.component.ts
+++ frontend/scheduler/src/app/create-announcement/create-announcement.component.ts
@@ -3,6 +3,7 @@ import { DatePipe } from '@angular/common';
 import { UserService } from './../services/user.service';
 import { Announcement } from './../../models/announcement.model'
 import { User } from './../../models/user.model'
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-create-announcement',
@@ -17,6 +18,7 @@ export class CreateAnnouncementComponent implements OnInit {
   constructor(
     private userService: UserService,
     private datePipe: DatePipe,
+    private snackBar: MatSnackBar,
     ) { 
   }
 
@@ -25,8 +27,26 @@ export class CreateAnnouncementComponent implements OnInit {
   }
 
   makeAnnouncement(){
-    console.log("made announcement!");
-    this.initialiseAnnouncement();
+    let errorMsg : string = "Something went wrong with making announcement, please try again later!";
+    this.userService.makeAnnouncements(this.newAnnouncement)
+    .subscribe(
+      response => {
+        if(JSON.parse(response).success){
+          //snackbar
+          this.snackBar.open("Announcement made!", null, {duration: 1000});
+          this.initialiseAnnouncement(); 
+        }else {
+          console.log(this.newAnnouncement);
+          this.snackBar.open(errorMsg, null, {duration: 1000});
+          console.log(response);
+        }
+      }, 
+      error => {
+        this.snackBar.open(errorMsg, null, {duration: 1000});
+        console.log("Server error in making announcement");
+        console.log(error);
+      }
+    )
   }
 
   initialiseAnnouncement(){
