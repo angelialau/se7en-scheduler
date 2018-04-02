89365e706ce4a98fa959a6702eb37a7ced6504c3
nosyarlin <rayson.ljk@gmail.com>
2018-04-02 23:45:49 +0800

    added appeals model and controller

---
+++ backend/controllers/Appeals.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var Appeal = require("../models/Appeal");
+var utils = require("../utils/utilities");
+var router = express.Router();
+
+// defining create appeal route
+router.post('/', function(req, res, next) {
+	// check if necessary params are available
+	if (utils.compareJSONKeys(req.body, Appeal.createStructure)) {
+		// create appeal
+		Appeal.createAppeal(req.body, function(err, count) {
+			utils.basicPostCallback(res, err, count);
+		})
+	} else {
+		res.json({"success":false, "message":"post params incomplete"});
+	}
+});
+
+// defining get appeal by id route
+router.get('/:id(\\d+)', function(req, res, next) {
+	if (req.params.id) {
+		Appeal.getAppealById(req.params.id, function(err, rows) {
+			utils.basicGetCallback(res, err, rows, 0);
+		});
+	} else {
+		// if no id given, return all rows
+		Appeal.getAppeals(function(err, rows) {
+			utils.basicGetCallback(res, err, rows, null);
+		});
+	}
+});
+
+// defining get appeals by instructor id route
+router.get('/Instructor/:id(\\d+)', function(req, res, next) {
+	if (req.params.id) {
+		Appeal.getAppealByInstructorId(req.params.id, function(err, rows) {
+			utils.basicGetCallback(res, err, rows, null);
+		});
+	}
+});
+
+// defining get appeals by pillar route
+router.get('/Pillar/:pillar(\\D+)', function(req, res, next) {
+	if (req.params.pillar) {
+		Appeal.getAppealsByPillar(req.params.pillar, function(err, rows) {
+			utils.basicGetCallback(res, err, rows, null);
+		});
+	}
+});
+
+// defining delete appeal route
+router.post('/Delete/:id(\\d+', function(req, res, next) {
+	if (req.params.id) {
+		Appeal.deleteAppeal(req.params.id, function(err, count) {
+			utils.basicPostCallback(res, err, count);
+		});
+	}
+});
+
+module.exports = router;
---
+++ backend/models/Appeal.js
@@ -0,0 +1,64 @@
+var db = require("../utils/dbconnection"); //reference to db connection
+
+var TABLE_NAME = "appeals";
+var COLUMN_ID = "id";
+var COLUMN_TITLE = "title";
+var COLUMN_CONTENT = "content";
+var COLUMN_DATE = "date";
+var COLUMN_INSTRUCTOR = "instructor";
+var COLUMN_INSTRUCTOR_ID = "instructorId";
+var COLUMN_PILLAR = "pillar";
+
+var Appeal = {
+	createStructure:{
+		title:null,
+		content:null,
+		instructor:null,
+		instructorId:null,
+		pillar:null
+	},
+	createAppeal:function(data, callback) {
+		return db.query("INSERT INTO " + 
+			TABLE_NAME + "(`" +
+			COLUMN_TITLE + "`,`" +
+			COLUMN_CONTENT + "`,`" +
+			COLUMN_INSTRUCTOR + "`,`" + 
+			COLUMN_INSTRUCTOR_ID + "`,`" +
+			COLUMN_PILLAR + "`)`" +
+			" VALUES(?,?,?,?,?)",
+			[data.title,
+			 data.content,
+			 data.instructor,
+			 data.instructorId,
+			 data.pillar],
+			 callback);
+	},
+	getAppealById:function(id, callback) {
+		return db.query("SELECT * FROM " + TABLE_NAME + 
+						" WHERE " + COLUMN_ID "=?", 
+						[id],
+						callback);
+	},
+	getAppealByInstructorId:function(id, callback) {
+		return db.query("SELECT * FROM " + TABLE_NAME + 
+						" WHERE " + COLUMN_INSTRUCTOR_ID "=?", 
+						[id],
+						callback);
+	},
+	getAppeals:function(callback) {
+		return db.query("SELECT * FROM " + TABLE_NAME + 
+						" WHERE 1",[],callback);
+	},
+	getAppealsByPillar:function(pillar, callback) {
+		return db.query("SELECT * FROM " + TABLE_NAME + 
+						" WHERE " + COLUMN_PILLAR + "=?",
+						[pillar],
+						callback);
+	},
+	deleteAppeal:function(id, callback) {
+		return db.query("DELETE FROM " + TABLE_NAME + " WHERE `" + COLUMN_ID +
+						"` =?", [id], callback);
+	}
+};
+
+module.exports=Appeal;
