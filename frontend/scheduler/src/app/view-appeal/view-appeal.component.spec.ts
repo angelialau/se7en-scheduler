b68ed16fdbd6783ce9f6f0f3926c462ea0a7145e
Yee Ting <yeeting_tham@mymail.sutd.edu.sg>
2018-04-02 21:31:32 +0800

    made a view appeal component and some stuff

---
+++ frontend/scheduler/src/app/view-appeal/view-appeal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewAppealComponent } from './view-appeal.component';
+
+describe('ViewAppealComponent', () => {
+  let component: ViewAppealComponent;
+  let fixture: ComponentFixture<ViewAppealComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ViewAppealComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewAppealComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
