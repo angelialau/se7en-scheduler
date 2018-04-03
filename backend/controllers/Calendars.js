bf27af4a7328a19dbb1cc18c8d44f3c6ed98605c
nosyarlin <rayson.ljk@gmail.com>
2018-04-03 11:17:40 +0800

    testing get day api api endpoint

--- backend/controllers/Calendars.js
+++ backend/controllers/Calendars.js
@@ -44,7 +44,7 @@ router.get('/Pillar/:id(\\d+)/:pillar([A-Z]+)', function(req, res, next) {
 });
 
 // defining get day route
-router.get('/Filter/day.:day/', function(req, res, next) {
+router.get('/Filter/day.:day?/', function(req, res, next) {
 	if (req.params.day) {
 		console.log(req.params.day);
 	} else{
