414a42297bdd1a56448c344f43aecaa0071ca6f2
angelialau <angelialkm@gmail.com>
2018-04-03 18:53:40 +0800

    ui changes to centralise all containers

--- frontend/scheduler/src/app/view-appeal/view-appeal.component.spec.ts
+++ frontend/scheduler/src/app/view-appeal/view-appeal.component.spec.ts
@@ -1,24 +1,19 @@
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 // import { ViewAppealComponent } from './view-appeal.component';
-
 // describe('ViewAppealComponent', () => {
 //   let component: ViewAppealComponent;
 //   let fixture: ComponentFixture<ViewAppealComponent>;
-
 //   beforeEach(async(() => {
 //     TestBed.configureTestingModule({
 //       declarations: [ ViewAppealComponent ]
 //     })
 //     .compileComponents();
 //   }));
-
 //   beforeEach(() => {
 //     fixture = TestBed.createComponent(ViewAppealComponent);
 //     component = fixture.componentInstance;
 //     fixture.detectChanges();
 //   });
-
 //   it('should create', () => {
 //     expect(component).toBeTruthy();
 //   });
