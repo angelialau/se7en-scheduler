fa00ae940f3441f3f6fef72ed6e8fc71febcbb94
angelialau <angelialkm@gmail.com>
2018-04-05 03:57:03 +0800

    middle of sched details testing

--- frontend/scheduler/src/app/schedule-details/schedule-details.component.spec.ts
+++ frontend/scheduler/src/app/schedule-details/schedule-details.component.spec.ts
@@ -1,53 +1,95 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { NO_ERRORS_SCHEMA } from '@angular/core';
-// import { RouterModule, Router, ActivatedRoute } from '@angular/router';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { HttpClientModule, HttpClient } from '@angular/common/http';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatSnackBarModule } from '@angular/material';
-// import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
-// import { RouterTestingModule } from '@angular/router/testing';
+// general testing imports 
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+// general imports 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+// component specific imports 
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { UserService } from './../services/user.service';
+import { ScheduleService } from './../services/schedule.service';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar, MatSnackBarModule, MatSnackBarConfig, MatSnackBarRef, 
+  SimpleSnackBar } from '@angular/material';
+import { Course } from './../../models/course.model';
+import { courseSorted, courseUnsorted, courseTestInput } from './test';
+import { Schedule } from './../../models/schedule.model';
 
-// import { Schedule } from './../../models/schedule.model';
-// import { User } from './../../models/user.model';
-// import { UserService } from './../services/user.service';
-// import { Course, Session, CourseDetail, courseDetails, class_type, 
-//   durations } from './../../models/course.model';
-// import { ScheduleService } from './../services/schedule.service';
+import { ScheduleDetailsComponent } from './schedule-details.component';
 
-// import { ScheduleDetailsComponent } from './schedule-details.component';
+describe('ScheduleDetailsComponent', () => {
+  let component: ScheduleDetailsComponent;
+  let fixture: ComponentFixture<ScheduleDetailsComponent>;
+  let userServiceStub : MockUserService;
+  let testBedUserService : MockUserService;
+  let scheduleServiceStub : MockScheduleService;
+  let testBedScheduleService: MockScheduleService;
+  let snackBar : MatSnackBar;
 
-// describe('ScheduleDetailsComponent', () => {
-//   let component: ScheduleDetailsComponent;
-//   let fixture: ComponentFixture<ScheduleDetailsComponent>;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ScheduleDetailsComponent ],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,       
+      ],
+      providers: [ 
+        HttpClientModule, 
+        DatePipe,
+        {provide: ActivatedRoute, useValue: {snapshot: {params: {'schedule_id': '4'}}}},
+        {provide: UserService, useClass: MockUserService },
+        {provide: ScheduleService, useClass: MockScheduleService },
+       ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ScheduleDetailsComponent ],
-//       imports: [
-//         BrowserModule,
-//         HttpClientModule,
-//         BrowserAnimationsModule,
-//         MatSnackBarModule,
-//         FormsModule,
-//         ReactiveFormsModule,
-//         RouterTestingModule
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScheduleDetailsComponent);
+    component = fixture.componentInstance;
+    testBedUserService = TestBed.get(UserService);
+    userServiceStub = fixture.debugElement.injector.get(UserService);
+    snackBar = fixture.debugElement.injector.get(MatSnackBar);
+    testBedScheduleService = TestBed.get(ScheduleService);
+    scheduleServiceStub = fixture.debugElement.injector.get(ScheduleService);
 
-//       ],
-//       providers: [ UserService, ScheduleService, HttpClientModule, 
-//       {provide: ActivatedRoute, useValue: {snapshot: {params: {'schedule_id': '4'}}}} ],
-//       schemas: [ NO_ERRORS_SCHEMA ]
-//     })
-//     .compileComponents();
-//   }));
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ScheduleDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  //   expect(component.schedule_id).toBeDefined();
+  //   expect(component.generated).toBeUndefined();
+  //   expect(component.courseIDs).toBe([];)
+  //   expect(component.courses).toBe([];)
+  //   expect(component.showCourseList).toBeTruthy();
+  //   expect(component.showEventForm).toBeFalsy();
+  //   expect(component.showCourseForm).toBeFalsy();
+  // });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  // it('should have user service injected and instantiated', () => {
+  //   expect(userServiceStub instanceof MockUserService).toBeTruthy();
+  //   inject([UserService], (injectService: UserService) => {
+  //     expect(injectService).toBe(testBedUserService);
+  //   });
+  // });
+
+  // it('should have schedule service injected and instantiated', () => {
+  //   expect(scheduleServiceStub instanceof MockScheduleService).toBeTruthy();
+  //   inject([ScheduleService], (injectService: ScheduleService) => {
+  //     expect(injectService).toBe(testBedScheduleService);
+  //   });
+  // });
+});
