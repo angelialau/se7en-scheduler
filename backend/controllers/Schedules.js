c0453e965f740f77ac75860bb6aad08f3a99d10a
nosyarlin <rayson.ljk@gmail.com>
2018-04-05 17:39:28 +0800

    updated generator

--- backend/controllers/Schedules.js
+++ backend/controllers/Schedules.js
@@ -91,7 +91,7 @@ router.post('/Generate', function(req, res, next) {
 				var result = [];
 
 				child.stdout.on('data', function(data) {
-					console.log(data.toString());
+					console.log(data);
 					result.push(JSON.parse(data.toString().trim()));
 				});
 
