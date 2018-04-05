1f5648e50afb2d15a86511bfb4244928edbff553
nosyarlin <rayson.ljk@gmail.com>
2018-04-05 22:57:53 +0800

    temporarily reduce number of available rooms

--- backend/utils/utilities.js
+++ backend/utils/utilities.js
@@ -61,6 +61,8 @@ var utilities = {
 	/**
 	 * Moves date forward by one day. If hits a weekend, date moves straight
 	 * to Monday
+	 *
+	 * Does not return anything, the input date will be modified
 	 */
 	incrementDate:function(date) {
 		var day = date.getDay();
@@ -74,6 +76,8 @@ var utilities = {
 	/**
 	 * Converts an event entry, into a format that can be displayed on 
 	 * the full calendar
+	 *
+	 * Returns a JSONObject
 	 */
 	eventToFullCalendar:function(event){
 		var output = {};
@@ -140,51 +144,51 @@ var utilities = {
 	availableRooms:[
 		"Think Tank 1",
 		"Think Tank 2",
-		"Think Tank 3",
-		"Think Tank 4",
-		"Think Tank 5",
-		"Think Tank 6",
-		"Think Tank 7",
-		"Think Tank 8",
-		"Think Tank 9",
-		"Think Tank 10",
-		"Think Tank 11",
-		"Think Tank 12",
-		"Think Tank 13",
-		"Think Tank 14",
-		"Think Tank 15",
-		"Think Tank 16",
-		"Think Tank 17",
-		"Think Tank 18",
-		"Think Tank 19",
-		"Think Tank 20",
-		"Think Tank 21",
-		"Think Tank 22",
-		"Think Tank 23",
-		"Think Tank 24",
-		"Think Tank 25",
-		"Think Tank 26",
-		"Cohort Classroom 1",
-		"Cohort Classroom 2",
-		"Cohort Classroom 3",
-		"Cohort Classroom 4",
-		"Cohort Classroom 5",
-		"Cohort Classroom 6",
-		"Cohort Classroom 7",
-		"Cohort Classroom 8",
-		"Cohort Classroom 9",
-		"Cohort Classroom 10",
-		"Cohort Classroom 11",
-		"Cohort Classroom 12",
-		"Cohort Classroom 13",
-		"Cohort Classroom 14",
-		"Cohort Classroom 15",
-		"Cohort Classroom 16",
-		"Lecture Theatre 1",
-		"Lecture Theatre 2",
-		"Lecture Theatre 3",
-		"Lecture Theatre 4",
-		"Lecture Theatre 5"
+		"Think Tank 3"
+		// "Think Tank 4",
+		// "Think Tank 5",
+		// "Think Tank 6",
+		// "Think Tank 7",
+		// "Think Tank 8",
+		// "Think Tank 9",
+		// "Think Tank 10",
+		// "Think Tank 11",
+		// "Think Tank 12",
+		// "Think Tank 13",
+		// "Think Tank 14",
+		// "Think Tank 15",
+		// "Think Tank 16",
+		// "Think Tank 17",
+		// "Think Tank 18",
+		// "Think Tank 19",
+		// "Think Tank 20",
+		// "Think Tank 21",
+		// "Think Tank 22",
+		// "Think Tank 23",
+		// "Think Tank 24",
+		// "Think Tank 25",
+		// "Think Tank 26",
+		// "Cohort Classroom 1",
+		// "Cohort Classroom 2",
+		// "Cohort Classroom 3",
+		// "Cohort Classroom 4",
+		// "Cohort Classroom 5",
+		// "Cohort Classroom 6",
+		// "Cohort Classroom 7",
+		// "Cohort Classroom 8",
+		// "Cohort Classroom 9",
+		// "Cohort Classroom 10",
+		// "Cohort Classroom 11",
+		// "Cohort Classroom 12",
+		// "Cohort Classroom 13",
+		// "Cohort Classroom 14",
+		// "Cohort Classroom 15",
+		// "Cohort Classroom 16",
+		// "Lecture Theatre 1",
+		// "Lecture Theatre 2",
+		// "Lecture Theatre 3",
+		// "Lecture Theatre 4",
+		// "Lecture Theatre 5"
 	],
 
 	zeroTime: new Date("1995-11-03 08:30")
